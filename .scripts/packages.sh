607242f8938ece82d6aa6c5395eb6d3cc6b19343
ajn123 <ajn123@vt.edu>
2020-05-20 21:17:21 -0500

    added packages

---
+++ .scripts/packages.sh
@@ -0,0 +1,21 @@
+
+
+
+
+
+
+
+BASE_PACKAGES = (
+  'python2',
+  'python3',
+  'python2-pip',
+  'python-pip'
+  'ruby',
+  'neofetch',
+  'bat',
+  'vim',
+  'tmux',
+  'neovim',
+  'zsh',
+  'zsh-theme-powerlevel9k'
+)
